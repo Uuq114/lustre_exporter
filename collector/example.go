009d588c9dc2310ee9182e6eb3562dbaeb59ebf7
Uuq114 <jiajie.sheng@sjtu.edu.cn>
2024-06-12 14:00:32 +0800

    export multiple metrics

--- collector/example.go
+++ collector/example.go
@@ -7,14 +7,16 @@ import (
 )
 
 type ExampleCollector struct {
-	randomFloatMetric *prometheus.Desc
-	logger            log.Logger
+	randomFloatMetric1 *prometheus.Desc
+	randomFloatMetric2 *prometheus.Desc
+	logger             log.Logger
 }
 
 func NewExampleCollector(logger log.Logger) Collector {
 	return &ExampleCollector{
-		randomFloatMetric: prometheus.NewDesc("random_float_metric", "example metric", nil, nil),
-		logger:            logger,
+		randomFloatMetric1: prometheus.NewDesc("random_float_metric1", "example metric", nil, nil),
+		randomFloatMetric2: prometheus.NewDesc("random_float_metric2", "example metric", nil, nil),
+		logger:             logger,
 	}
 }
 
@@ -23,9 +25,11 @@ func init() {
 }
 
 func (c *ExampleCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- c.randomFloatMetric
+	ch <- c.randomFloatMetric1
+	ch <- c.randomFloatMetric2
 }
 
 func (c *ExampleCollector) Collect(ch chan<- prometheus.Metric) {
-	ch <- prometheus.MustNewConstMetric(c.randomFloatMetric, prometheus.GaugeValue, rand.Float64())
+	ch <- prometheus.MustNewConstMetric(c.randomFloatMetric1, prometheus.GaugeValue, rand.Float64())
+	ch <- prometheus.MustNewConstMetric(c.randomFloatMetric2, prometheus.GaugeValue, rand.Float64())
 }
